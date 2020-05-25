0b67e0ab6182b4a21148322f07a92eb74f3f53bb
GermanBluefox <dogafox@gmail.com>
2020-05-25 14:54:09 +0200

    Updates

---
+++ precache-manifest.aa945c1ba759529183e0a24ae03b2b3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9959113c0eded7de9794d96a26c1d4a7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8e3b2f9f6d8412fb18ad",
+    "url": "./static/css/main.18486c09.chunk.css"
+  },
+  {
+    "revision": "d2dbde59751c8ced3472",
+    "url": "./static/js/2.50ebcd2c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.50ebcd2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e3b2f9f6d8412fb18ad",
+    "url": "./static/js/main.c552a29c.chunk.js"
+  },
+  {
+    "revision": "8d73c22166bfb0401e91",
+    "url": "./static/js/runtime-main.b3fc422b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6a8e350cabdd0ee6e49b311755bd9370.js"
+  "./precache-manifest.aa945c1ba759529183e0a24ae03b2b3b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
